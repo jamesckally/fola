989b74f683b375e8ef29088ea4be05981712b3d7
Stephen Oluwatobi Akanbi <Stephenoluwatobi30@gmail.com>
2025-12-04 23:06:43 +0100

    Fix Header.tsx build error - replace Toggle with Switch component

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import { Switch } from '@/components/ui/switch';
 
 export default function Header() {
     return (
         <div className="flex justify-between items-center mb-6">
             <Image src="/logo.svg" alt="SwapaWallet" width={140} height={40} />
-            <Toggle checked={true} onChange={() => { }} />
+            <Switch checked={true} onCheckedChange={() => { }} />
         </div>
     );
 }
